a7f4d1ebedb60a15bebd0cd514ce5712eb50bcf2
Ben Bassaken <bassakendev@gmail.com>
2024-01-29 14:13:42 +0100

    update-remote-repository

---
+++ src/components/login/form.php
@@ -0,0 +1,16 @@
+<form action="#" method="post">
+    <div class="header"><span class="shine">D</span>U<span class="shine">M</span>P<span class="shine">I</span></div>
+    <div class="body">
+        <div class="input-form">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="" placeholder="Email...">
+        </div>
+        <div class="input-form">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="" placeholder="Password...">
+        </div>
+    </div>
+    <div class="footer">
+        <button type="submit">Login</button>
+    </div>
+</form>
